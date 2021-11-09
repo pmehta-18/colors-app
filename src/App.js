11c8d52166f06b274c1ccc38b7b63e679b06f60e
Pooja Mehta <poojam018@gmail.com>
2021-11-09 19:30:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useEffect, useState } from "react";
+import { getColorShades, getData } from "./utils/helper";
 
-function App() {
+export default function App() {
+  const [word, setWord] = useState("");
+  const [data, setData] = useState({});
+  const [colors, setColors] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const result = await getData();
+      setData(result);
+    }
+    fetchData();
+  }, []);
+  const handleClick = () => {
+    if (data[word]) {
+      // find color shades
+      setColors(getColorShades(data[word]));
+    }
+    else {
+      setColors([]);
+      window.alert('Word not found!');
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={word} onChange={(e) => setWord(e.target.value)} />
+      <button onClick={handleClick} className="action-button">Find colors</button>
+      <div className="color-grid">
+        {colors.map((color) => {
+          return <div className="color--grid-item" key={color.hex} style={{backgroundColor: color.hex}}>
+            <div className="color-code" style={{color: data[word]}}>{color.hex}</div>
+          </div>
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
